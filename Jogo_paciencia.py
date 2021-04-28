8a384acdfebc38e5738a4b2a45ad880e5d753373
ArthurPansini <arthurpansini35@gmail.com>
2021-04-28 00:57:06 -0300

    Extrair cartas

--- Jogo_paciencia.py
+++ Jogo_paciencia.py
@@ -1,3 +1,13 @@
 def cria_baralho():
     listabaralho = ['A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣']
     return listabaralho
+
+def extrai_naipe (carta):
+    if '♦' in carta:
+        return '♦'
+    elif '♥' in carta:
+        return '♥'
+    elif '♣' in carta:
+        return '♣'
+    else:
+        return '♠'
