92b4ecb67e7cbd366615fe6f2d43cf29d855c89d
ArthurPansini <arthurpansini35@gmail.com>
2021-05-11 01:52:07 -0300

    Primeiro protótipo do baralho com correções

--- Jogo_paciencia.py
+++ Jogo_paciencia.py
@@ -1,3 +1,5 @@
+import random
+
 def cria_baralho():
     listabaralho = ['A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣']
     return listabaralho
@@ -71,10 +73,53 @@ iniciar = input("Aperte [Enter] para iniciar o jogo...")
 
 print('O estado atual do baralho é:\n' )
 
-print(cria_baralho())
-baralho = cria_baralho()
-escolhercarta = input('Escolha uma carta (digite um número entre 1 e 52):')
-escolhercarta = int(escolhercarta)
-carta = baralho[escolhercarta - 1]
+resposta = 'sim'
+while resposta == "sim":
+    baralho = cria_baralho()
+    embaralhando = (random.sample(baralho, 52))
+    while possui_movimentos_possiveis(embaralhando) != False:
+        for i in range (len(embaralhando)):
+            print ('{0}. {1}'.format(i+1, embaralhando[i]))
+
+        escolhercarta = int(input('Escolha uma carta (digite um número entre 1 e 52):'))
+        possivel = lista_movimentos_possiveis(embaralhando, escolhercarta - 1)
+        if len(possivel) == 0:
+            print('A carta {} não pode ser movida.'.format(embaralhando[escolhercarta - 1]))
+            escolhercarta = int(input('Escolha uma carta (digite um número entre 1 e {}):'.format(len(embaralhando))))
+        elif len(possivel) == 2:
+            print('Sobre qual carta você quer empilhar o {}?'.format(embaralhando[escolhercarta - 1]))
+            print('{0}. {1}'.format(1, embaralhando[escolhercarta - 2]))
+            print('{0}. {1}'.format(2, embaralhando[escolhercarta - 4]))
+            escolha2 = ""
+            while escolha2 != '1' and escolha2 != '2':
+                    escolha2 = input('Digite o número de sua escolha (1 ou 2): ')
+                    if escolha2 == '1':
+                        embaralhando = empilha(embaralhando, escolhercarta-1, escolhercarta-2)
+                        print('o estado atual do baralho é:')
+                    elif escolha2 == '2':
+                        embaralhando = empilha(embaralhando, escolhercarta-1, escolhercarta-4)
+                        print('o estado atual do baralho é:')
+                    else:
+                        print('Escolha inválida!')
+                        print("Escolha sobra qual carta você que empilhar o {}".format(embaralhando[escolhercarta-1]))
+                        print('{0}. {1}'.format(1, embaralhando[escolhercarta-2]))
+                        print('{0}. {1}'.format(2, embaralhando[escolhercarta-4]))
+
+        elif len(possivel) == 1:
+                embaralhando = empilha(embaralhando, escolhercarta-1, escolhercarta-2)
+                print('o estado atual do baralho é:')
+        possui_movimentos_possiveis(embaralhando) == False
+    if len(embaralhando) == 1:
+        print('Parabéns você ganhou!')
+        resposta = input('Você quer jogar novamente? (sim ou não) ')
+    else:
+        print('Que pena você perdeu. Tente outra vez!')
+        resposta = input('Você quer jogar novamente? (sim ou não) ')
+        
+
+
+
+
+
     
         
