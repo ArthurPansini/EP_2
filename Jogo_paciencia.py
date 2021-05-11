4eaf691604b7b9ac6576223a117fc01d866c6d55
ArthurPansini <arthurpansini35@gmail.com>
2021-05-10 22:59:41 -0300

    Introdução do jogo

--- Jogo_paciencia.py
+++ Jogo_paciencia.py
@@ -47,5 +47,24 @@ def possui_movimentos_possiveis(listastr):
 
     else:
         return False
+
+print("Paciência Acordeão\n") 
+print("==================\n")
+
+print("Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.\n") 
+
+print("Existem apenas dois movimentos possíveis:\n") 
+
+print("1. Empilhar uma carta sobre a carta imediatamente anterior;\n") 
+print("2. Empilhar uma carta sobre a terceira carta anterior.\n") 
+
+print("Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:\n")
+
+print("1. As duas cartas possuem o mesmo valor ou\n")
+print("2. As duas cartas possuem o mesmo naipe.\n")
+
+print("Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.\n")
+
+print("Aperte [Enter] para iniciar o jogo...\n")
     
         
